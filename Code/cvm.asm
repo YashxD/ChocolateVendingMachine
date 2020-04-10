8d41573699435b4757b0686dd407a91685f66774
Yash <yashjain.1999@gmail.com>
2020-04-10 19:36:16 +0530

    Update

--- Code/cvm.asm
+++ Code/cvm.asm
@@ -1,149 +1,214 @@
-#make_bin#
-
-#LOAD_SEGMENT=FFFFh#
-#LOAD_OFFSET=0000h#
-
-#CS=0000h#
-#IP=0000h#
-
-#DS=0000h#
-#ES=0000h#
-
-#SS=0000h#
-#SP=FFFEh#
-
-#AX=0000h#
-#BX=0000h#
-#CX=0000h#
-#DX=0000h#
-#SI=0000h#
-#DI=0000h#
-#BP=0000h#
-		
-			jmp ioinit
-			db     	5 dup(0)
-			dw		interrupt
-			dw		0000
-			db		1012 dup(0)
-			
-ioinit:		
+.model		tiny
+.data
 		porta		equ 	10h
 		portb		equ		12h
 		portc		equ		14h
 		ctrio		equ		16h
+		button		db		?
+		weight_need	db		82
+		motor 		db		00001100b
+		choc_no_1	db		10
+		choc_no_2	db		10
+		choc_no_3	db		10
 		
-		weight_need	equ		02h
-		motor		equ		03h
-		button		equ		04h
-		choc_no_1	equ		05h
-		choc_no_2	equ		06h
-		choc_no_3	equ		07h
+.code
+.startup
+init:
+       	mov ax,0
+		mov es,ax
+		mov ax,interrupt
+		mov [es:08h],ax
+		mov ax,00h
+		mov [es:0ah],ax
 		
-		mov ax,0100h
-        mov ds,ax
-        mov es,ax
-        mov ss,ax
-        mov sp,0FFFEh
+		mov       ax,0200h
+        mov       ds,ax
+        mov       es,ax
+        mov       ss,ax
+        mov       sp,0FFFEH
+		  
+		mov al,10010001b
+		out	ctrio,al
+
+		mov choc_no_1,10
+		mov choc_no_2,10
+		mov choc_no_3,10
+		
+x1:		jmp x1		
+
+interrupt:
+		call delay	
+		;call testmot
+		;jmp choc1
 		
 		mov al,10010001b
 		out	ctrio,al
 		
-		mov al,10
-		mov choc_no_1,al
-		mov choc_no_2,al
-		mov choc_no_3,al
-		
-x1:		jmp x1
-
-interrupt:
-		
-		call delay		
-		in al,portc		;stores state of buttons in al then checks if a button has been pressed
+		in al,portc		;state of buttons in al
 		and al,07h
 		cmp al,07h
 		jz exitp
 		
 		call delay
-		in al,portc		;stores state of buttons in al then checks if a button has been actually pressed
+		in al,portc		;state of buttons in al
 		and al,07h
 		mov button,al
 		cmp al,07h
 		jz exitp
 		
-		mov di,05h
-		mov motor,00001100b
 		
+		;Initialize Variables
+		mov weight_need, 82
+		mov motor, 00000001b
+		lea di,choc_no_1
+
 but1:					;button 1 was pressed the weight of the coins needed is stored
 		cmp button,06h
 		jnz but2
-		mov weight_need,
-		jmp x3
+		jmp weight_ready
 		
 but2:					;button 2 was pressed the weight of the coins needed is stored and the motor selected
 		cmp button,05h
 		jnz but3
-		mov weight_need,
+		mov weight_need,89
 		xor motor,00010000b
 		inc di
-		jmp x3
+		jmp weight_ready
 		
 but3:					;button 3 was pressed the weight of the coins needed is stored and the motor selected
 		cmp button,03h
 		jnz exitp
-		mov weight_need,
+		mov weight_need,103
 		xor motor,00100000b
 		inc di
 		inc di
-x3:		
-		mov bx,1770h
+		
+		
+weight_ready:
 
-twomindelay:			
-		call delay
-		dec bx
-		jnz twomindelay
+		;call delay2min
+		
+		in al,portc	
+		and al,00001000b
+		;jz weight_ready
+		
+		call rotmot
+		;call testmot
 		
 		in al,porta
 		cmp al,weight_need			;equal or more coin
 		jb exitp
-		
-		out portb,motor
-		xor motor,00001010b
-		out portb,motor				;motor turned by one step
-		xor motor,00000101b
-		out portb,motor
-		xor motor,00001010b
-		out portb,motor
-		
-		sub [di],1
+				
+		;call rotmot
+		sub byte ptr [di],1
 		
 choc1:								;checking if chocolate 1 finished
-		cmp [05h],0
-		jnz choc2
-		mov al,00001111
+		cmp choc_no_1,0
+		jne choc2
+		mov al,00001111b
 		out ctrio,al
 choc2:								;checking if chocolate 2 finished
-		cmp [06h],0
-		jnz choc3
-		mov al,00001101
+		cmp choc_no_2,0
+		jne choc3
+		mov al,00001101b
 		out ctrio,al
 choc3:								;checking if chocolate 3 finished
-		cmp [07h],0
-		jnz exitp
-		mov al,00001011
+		cmp choc_no_3,0
+		jne exitp
+		mov al,00001011b
 		out ctrio,al
 		
-exitp:		iret		
+exitp:		
+		mov al,10010001b
+		out	ctrio,al
+		iret		
 		
 .exit
 
+
 delay proc near
 		mov cx,30d4h
-	x2:	nop							;20ms delay
+	x2:	nop
 		nop
 		nop
 		nop
 		loop x2
 		ret
 delay endp
+
+
+delay2min	proc near
+		mov cx,1770
+delay2minloop:
+		call delay
+		loop delay2minloop
+		ret
+delay2min	endp
+
+
+delayr	proc near
+		mov cx,0F000h
+delayrr:
+		nop
+		nop
+		nop
+		nop
+		loop delayrr
+		ret
+delayr	endp
+
+
+rotmot proc near
+		;and motor,00000000b
+		;or	motor,00000001b
+		mov al,motor
+		out portb,al
+		call delayr
+		
+		xor motor,00000011b
+		mov al,motor
+		out portb,al
+		call delayr
+		
+		xor motor,00000110b
+		mov al,motor
+		out portb,al
+		call delayr
+		
+		xor motor,00001100b
+		mov al,motor
+		out portb,al
+		call delayr
+		
+		xor motor,00001001b
+		mov al,motor
+		out portb,al
+		call delayr
+		ret
+rotmot	endp
+
+
+testmot proc near
+		mov al,00000001b
+		out portb,al
+		call delayr
+		
+		mov al,00000010b
+		out portb,al				;motor turned by one step
+		call delayr
+		
+		mov al,00000100b
+		out portb,al
+		call delayr
+		
+		mov al,00001000b
+		out portb,al
+		call delayr
+		
+		mov al,00000001b
+		out portb,al
+		call delayr
+		ret
+testmot endp
 
 end
